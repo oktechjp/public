c51d3906f006a544fe2cbcb6394cae6f9228cdbc
Martin Heidegger <martin.heidegger@gmail.com>
2025-09-29 11:42:11 +0900

    removing corners

--- scripts/build/lib/prepare-photo.mjs
+++ scripts/build/lib/prepare-photo.mjs
@@ -1,16 +1,4 @@
-import sharp from "sharp";
 import { join } from "node:path";
-
-function toHex(col) {
-  return col.toString(16).padStart(2, "0");
-}
-
-function toHexColor(rgbs, i) {
-  return `${rgbs
-    .slice(i, i + 3)
-    .map(toHex)
-    .join("")}`;
-}
 
 export async function preparePhoto({
   cwd,
@@ -20,39 +8,4 @@ export async function preparePhoto({
 }) {
   const src = join(cwd, target.file);
   copies.push({ src, target: join(targetFolder, target.file) });
-  if (!target.corners) {
-    try {
-      const base = await sharp(src)
-        .resize({
-          width: 80,
-          height: 80,
-          fit: "fill",
-        })
-        .normalize()
-        .modulate({
-          saturation: 1.5,
-        })
-        .toColorspace("srgb")
-        .toFormat("tiff")
-        .toBuffer();
-      const x3y3 = Array.from(
-        await sharp(base)
-          .resize({
-            width: 3,
-            height: 3,
-            fit: "fill",
-          })
-          .toFormat("raw")
-          .toBuffer(),
-      );
-      target.corners = [
-        toHexColor(x3y3, 0),
-        toHexColor(x3y3, 2 * 3),
-        toHexColor(x3y3, 6 * 3),
-        toHexColor(x3y3, 8 * 3),
-      ];
-    } catch (cause) {
-      throw new Error(`Unable to process corners for ${src}`, { cause });
-    }
-  }
 }
